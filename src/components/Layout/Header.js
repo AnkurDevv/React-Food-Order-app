615595365a19474966dffa4aa8415d4e6626004e
Ankur Aggarwal <talkankur24@gmail.com>
2022-12-07 19:48:57 -0500

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import {Fragment} from "react";
+import classes from './Header.module.css';
+// extension below matters tells that it is not js file and therefore should be transmitted accordingly
+import mealsImage from '../../assets/meals.jpg'
+
+const Header = (props) =>{
+ return(
+  <Fragment>
+   <header className={classes.header}>
+    <h1>ReactMeals</h1>
+    <button>Cart</button>
+   </header>
+   {/* cant use dot notation since below className has - in its name */}
+   <div className={classes['main-image']}>
+    <img src={mealsImage} alt="table full of food" />
+   </div>
+  </Fragment>
+ )
+}
+
+export default Header;
