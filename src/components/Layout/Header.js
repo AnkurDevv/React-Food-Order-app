e6986ffe8a3a3c40fbadabad3faeddce51763d33
Ankur Aggarwal <talkankur24@gmail.com>
2022-12-08 18:26:12 -0500

    added the cart icon component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,15 @@
 import {Fragment} from "react";
 import classes from './Header.module.css';
 // extension below matters tells that it is not js file and therefore should be transmitted accordingly
-import mealsImage from '../../assets/meals.jpg'
+import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) =>{
  return(
   <Fragment>
    <header className={classes.header}>
     <h1>ReactMeals</h1>
-    <button>Cart</button>
+     <HeaderCartButton/>
    </header>
    {/* cant use dot notation since below className has - in its name */}
    <div className={classes['main-image']}>
