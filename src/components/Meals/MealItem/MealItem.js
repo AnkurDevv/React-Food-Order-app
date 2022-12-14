3782244b2bfeb37aad218ee32fa43470c86fc70b
Ankur Aggarwal <talkankur24@gmail.com>
2022-12-13 19:43:58 -0500

    added the add and remove functionality for cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,22 @@
 import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
+import { useContext } from 'react';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
   // fixed to makes sure that we always render 2 decimal places
-  const price = `$${props.price.toFixed(2)}`
+  const price = `$${props.price.toFixed(2)}`;
+  const cartCtx = useContext(CartContext);
+
+  const addItemToCartHandler = amount =>{
+   cartCtx.addItem({
+    id: props.id,
+    name: props.name,
+    amount: amount,
+    price: props.price
+   })
+  }
+
   return (
     <li className={classes.meal}>
       <div>
@@ -12,7 +25,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id}/>
+        <MealItemForm id={props.id} onAddToCart={addItemToCartHandler}/>
       </div>
     </li>
   )
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,87 @@
 import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+ items: [],
+ totalAmount: 0
+
+}
+
+const cartReducer = (state, action) =>{
+
+ // LOGIC TO ADD AN ITEM - START
+ if(action.type === 'ADD'){
+   console.log('reaching add ')
+   // concat : adds the item to an array but unlike push it doesnt edit the existing array but rtn a new array
+   // const updatedItems = state.items.concat(action.item);
+   const updatedTotalAmount =
+     state.totalAmount + action.item.price * action.item.amount
+   // will rtn index or null : if the item we are looking for in an array has the same id as action dispatched rtn index
+   const existingCartItemIndex = state.items.findIndex(
+     (item) => item.id === action.item.id
+   )
+   const existingCartItem = state.items[existingCartItemIndex]
+
+   let updatedItems
+
+   if (existingCartItem) {
+     const updatedItem = {
+       ...existingCartItem,
+       amount: existingCartItem.amount + action.item.amount,
+     }
+     updatedItems = [...state.items]
+     // get the already existing item from the cart and override it with this existingCartItem to avoid duplication
+     updatedItems[existingCartItemIndex] = updatedItem
+   } else {
+     // if it doesnt exist that is existingCartItem was null aka falsy then simply just add it
+     updatedItems = state.items.concat(action.item)
+   }
+   // LOGIC TO ADD AN ITEM - END
+
+   return {
+    items: updatedItems,
+    totalAmount: updatedTotalAmount,
+   }
+  }
+  // LOGIC TO REMOVE AN ITEM - START
+  if (action.type === 'REMOVE') {
+   console.log('reaching remove item');
+   const existingCartItemIndex = state.items.findIndex(item => item.id === action.id);
+   const existingItem = state.items[existingCartItemIndex];
+   const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+   let updatedItems;
+   if(existingItem.amount === 1){
+    updatedItems = state.items.filter(item => item.id !== action.id);
+   }else{
+     const updatedItem = {...existingItem, amount: existingItem.amount-1 };
+     updatedItems = [...state.items];
+     updatedItems[existingCartItemIndex] = updatedItem;
+   }
+   return {
+    items:updatedItems,
+    totalAmount: updatedTotalAmount
+   }
+  }
+  // LOGIC TO REMOVE AN ITEM - END
+ // your reducer function needs to rtn a default state
+ return defaultCartState
+}
 
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {}
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
-  const removeItemFromCartHandler = (id) => {}
+  const addItemToCartHandler = (item) => {
+   dispatchCartAction({type:'ADD', item:item})
+  }
+
+  const removeItemFromCartHandler = (id) => {
+   dispatchCartAction({ type: 'REMOVE', id: id })
+  }
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   }
