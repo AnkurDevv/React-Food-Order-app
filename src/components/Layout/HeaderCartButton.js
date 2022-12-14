30249a9a66e0276f36063f9524f47c83c75d3240
Ankur Aggarwal <talkankur24@gmail.com>
2022-12-14 17:00:06 -0500

    added some special effects to headerCart Btn

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,33 @@
 import classes from './HeaderCartButton.module.css'
 import CartIcon from '../Cart/CartIcon';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = props =>{
+ const [btnHighlighted, setBtnHighLighted] = useState(false);
  const cartctx = useContext(CartContext);
- const numberOfCartItems = cartctx.items.reduce((currentNum,item)=>{
+ const {items} = cartctx;
+ const numberOfCartItems = items.reduce((currentNum,item)=>{
   return currentNum + item.amount;
  },0);
+ 
+ const btnClasses = `${classes.button} ${btnHighlighted ? classes.bump: ''}`;
+
+ useEffect(() => {
+   if (cartctx.items.length === 0) {
+     return
+   }
+   setBtnHighLighted(true)
+   const timer = setTimeout(() => {
+     setBtnHighLighted(false);
+   }, 300);
+   return () => {
+    clearTimeout(timer);
+   }
+ }, [items])
 
  return (
-   <button className={classes.button} onClick={props.onClick}>
+   <button className={btnClasses} onClick={props.onClick}>
      <span className={classes.icon}>
        <CartIcon></CartIcon>
      </span>
