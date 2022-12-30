6c586d85495a8b31c957ffe20f96b7366de1173c
Ankur Aggarwal <talkankur24@gmail.com>
2022-12-29 19:55:54 -0500

    data is now coming through firebase api not static anymore

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -2,13 +2,15 @@ import classes from './Input.module.css'
 import React from 'react';
 
 const Input = React.forwardRef((props, ref) => {
+  let isInputNumber =  props.input.type == 'number';
+  let inputClass = isInputNumber ? classes.inputNumber: '';
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
       {/* ...props.input to ensure that all the necessary props are being added */}
-      <input ref={ref} {...props.input} />
+      <input ref={ref} {...props.input} className={inputClass} />
     </div>
-  );
+  )
 });
 
 export default Input;
